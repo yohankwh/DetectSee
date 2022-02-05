b52ce969ce4ab07d04ea0adb58bb53ddab577d87
Yohan Kurnia Wijaya <yohanhuang300999@gmail.com>
2022-02-05 19:10:19 +0700

    [UPDATE] gitignore, package.json + plant photos

--- backend/api/router/mainRoute.js
+++ backend/api/router/mainRoute.js
@@ -1,20 +1,7 @@
 import express from 'express';
 import DiseasesController from '../controller/diseasesController.js';
 import PlantsController from '../controller/plantsController.js';
-import PredictionsController from '../controller/predictionsController.js';
 import ImagesController from '../controller/imagesController.js';
-import multer from 'multer';
-
-const fileStorage = multer.diskStorage({
-    destination: (req,res,cb)=>{
-        cb(null, 'uploads');
-    },
-    filename: (req,file,cb)=>{
-        cb(null, new Date().getTime()+'-'+file.originalname);
-    }
-})
-
-const upload = multer({storage:fileStorage});
 
 const router = express.Router();
 
@@ -25,11 +12,4 @@ router.route('/diseases/:name').get(DiseasesController.getDiseaseByParam);
 
 router.route('/plants').get(PlantsController.getPlants);
 router.route('/plants/:name').get(PlantsController.getPlantByParam);
-
-router.route('/predictions').get(PredictionsController.getPredictions)
-                 .post(upload.single('image_plant'),PredictionsController.postPrediction)
-                 .delete(PredictionsController.deletePrediction);
-
-router.route('/predictions/:id').get(PredictionsController.getPredictionById);
-
 export default router;
